a9528bc6996c163e6952433fe23634b25c11735a
Kevin Cooper <cooperka92@gmail.com>
2017-07-15 18:48:48 -0700

    Add an 'add' input and button to POST /api/heroes

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -28,6 +28,20 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().then((heroes) => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+
+    if (!name) {
+      return;
+    }
+
+    this.heroService.create(name)
+      .then((hero) => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
